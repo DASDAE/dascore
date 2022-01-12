f152917e05fb45beeaef2640f78f2178b9bf3662
Derrick Chambers <djachambeador@gmail.com>
2022-01-11 17:42:36 -0700

    start pickle

---
+++ tests/test_io/test_pickle.py
@@ -0,0 +1,27 @@
+"""
+Tests for reading/writing pickles.
+"""
+import pickle
+
+import pytest
+
+from dascore.io.pickle.core import _is_pickle
+
+
+@pytest.fixture(scope="session")
+def pickle_patch_path(tmp_path_factory, random_patch):
+    """Pickle a patch and return the path."""
+    path = tmp_path_factory.mktemp("pickle_test") / "test.pkl"
+    with path.open("wb") as fi:
+        pickle.dump(random_patch, fi)
+    return path
+
+
+class TestIsPickle:
+    """Test detecting pickle format."""
+
+    def test_detect_file(self, pickle_patch_path):
+        """Simple test on output of is pickle."""
+        out = _is_pickle(pickle_patch_path)
+        assert out
+        assert out[0] == "PICKLE"
