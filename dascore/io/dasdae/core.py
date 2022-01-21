7176d487ddcec6944f4479a7272b967f7d4ea2d7
Derrick Chambers <djachambeador@gmail.com>
2022-01-21 15:36:45 -0700

    update

--- dascore/io/dasdae/core.py
+++ dascore/io/dasdae/core.py
@@ -32,12 +32,53 @@ def generate_patch_node_name(patch):
     return f"DAS__{net}__{sta}__{tag}__{start}__{end}"
 
 
-def save_patch(patch, wave_group, h5):
+def _save_attrs_and_dim(patch, patch_group):
+    """Save the attributes."""
+    # copy attrs to group attrs
+    # TODO will need to test if objects are serializable
+    for i, v in patch.attrs.items():
+        patch_group._v_attrs[i] = v
+    patch_group._v_attrs["dims"] = ",".join(patch.dims)
+
+
+def _save_array(data, name, group, h5):
+    """Save an array to a group, handle datetime flubbery."""
+    # handle datetime conversions
+    is_dt = np.issubdtype(data, np.datetime64)
+    is_td = np.issubdtype(data, np.timedelta64)
+    if is_dt or is_td:
+        data = data
+
+    # out = h5.create_array(
+    #     group,
+    #     f"coord_{name}",
+    #     data,
+    # )
+
+
+def _save_coords(patch, patch_group, h5):
+    """Save coordinates"""
+    for name, coord in patch._data_array.coords.items():
+        _save_array(coord, name, patch_group, h5)
+
+        # array = patch.coords[name]
+        # out = h5.create_array(
+        #     patch_group,
+        #     f"coord_{name}",
+        #     array,
+        # )
+        # # breakpoint()
+
+
+def _save_patch(patch, wave_group, h5):
     """Save the patch to disk."""
     name = generate_patch_node_name(patch)
     patch_group = h5.create_group(wave_group, name)
-    for i, v in patch.attrs.items():
-        patch_group._v_attrs[i] = v
+    _save_attrs_and_dim(patch, patch_group)
+    _save_coords(patch, patch_group, h5)
+
+    # add data
+    h5.create_array(patch_group, "data", patch.data)
 
 
 class DASDAEIO(FiberIO):
@@ -56,4 +97,4 @@ class DASDAEIO(FiberIO):
             patches = [patch] if isinstance(patch, dc.Patch) else patch
             waveforms = h5.create_group(h5.root, "waveforms")
             for patch in patches:
-                save_patch(patch, waveforms, h5)
+                _save_patch(patch, waveforms, h5)
