f6b5ec9e87d1211c4c58a4b1e23cad7535372e3b
Ahmad <92008628+ahmadtourei@users.noreply.github.com>
2022-12-20 14:51:55 -0700

    Adding standardize function (#92)
    * Adding standardize function
    
    * Adding standardize function
    
    Co-authored-by: ahmadtourei <ahmadtourei@gmail.com>

--- dascore/proc/__init__.py
+++ dascore/proc/__init__.py
@@ -2,7 +2,7 @@
 Module to Patch Processing.
 """
 from .aggregate import aggregate
-from .basic import abs, normalize, rename, squeeze, transpose
+from .basic import abs, normalize, rename, squeeze, standardize, transpose
 from .detrend import detrend
 from .filter import median_filter, pass_filter
 from .resample import decimate, interpolate, iresample, resample
