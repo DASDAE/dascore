86d898451bb154ff0a8f494f7e2f2d61f556d4c5
Derrick Chambers <d-chambers@users.noreply.github.com>
2023-01-19 16:57:05 -0600

    Fix doc build (#109)
    Removed panflute, built custom filter

--- docs/filters/fill_links.py
+++ docs/filters/fill_links.py
@@ -1,19 +1,20 @@
 """
-Panflute filter that embeds wikipedia text
+Custom filter that makes dynamic (sphinx-esc) cross links work.
 
-Replaces markdown such as [Stack Overflow](wiki://) with the resulting text.
+Replaces markdown such as [Patch](`dascore.Patch`) with the path to the markdown
+file created from dascore/scripts/build_api_docs.py.
 """
-import fnmatch
+import io
 import json
-import warnings
+import re
+import sys
 from functools import cache
 from pathlib import Path
-
-import panflute as pf
+from typing import Dict
 
 
 @cache
-def get_cross_ref_dict():
+def get_cross_ref_dict() -> Dict[str, str]:
     """
     Load cross-reference dictionaries.
 
@@ -29,23 +30,61 @@ def get_cross_ref_dict():
     return out
 
 
-def action(elem, doc):
-    """callback to run on each element."""
-    if isinstance(elem, pf.Link) and fnmatch.fnmatch(elem.url, "%60*60"):
-        mapping = get_cross_ref_dict()
-        found_str = elem.url[3:-3]
-        if found_str not in mapping:
-            msg = f"{found_str} is not a listed cross reference! Aborting doc gen."
-            warnings.warn(msg)
-            # raise KeyError(msg)
-        else:
-            elem.url = mapping[found_str]
-        return elem
+def load_stdin(input_stream=None):
+    """
+    Load input from stdin (json string)
+    """
+    if input_stream is None:
+        input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
+    return input_stream.read()
 
 
-def main(doc=None):
-    return pf.run_filter(action, doc=None)
+def dump_stdout(output_str, out_stream=None):
+    """
+    Dump modified json string to std out.
+    """
+    if out_stream is None:
+        out_stream = sys.stdout
+    out_stream.write(output_str)
+
+
+def _has_link(raw_data, start, stop):
+    """Determine if the work 'link' occurs before start index."""
+    new_start = max([start - 15, 0])
+    return "link" in raw_data[new_start:stop]
+
+
+def replace_links(data):
+    reg_pattern = "(?<=\%60)(.*?)(?=\%60)"
+    for match in re.finditer(reg_pattern, data):
+        start, stop = match.span()
+        # look back for "link" otherwise this may just be an emphasis.
+        if not _has_link(data, start, stop):
+            continue
+        cross_refs = get_cross_ref_dict()
+        key = data[start:stop].replace("`", "")
+        new_value = cross_refs.get(key, key)
+        data = f"{data[:start]}{new_value}{data[stop:]}"
+    return data
+
+
+def test():
+    """Function to test filter."""
+    here = Path(__file__).parent
+    data_path = here / "filter_test_data" / "test.json"
+    with data_path.open("r") as fi:
+        data = fi.read()
+    input_stream = io.StringIO(data)
+    main(input_stream)
+
+
+def main(raw_data=None):
+    """Run filter."""
+    raw_data = load_stdin(raw_data)
+    output_data = replace_links(raw_data)
+    dump_stdout(output_data)
 
 
 if __name__ == "__main__":
+    # test()  # uncomment to test.
     main()
