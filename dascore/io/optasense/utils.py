e66f5f40caa497bb340ea925fe1153442333e8a1
JacksonZhu <zhudonglin1990@hotmail.com>
2023-05-14 11:32:56 -0600

    optasense_io

--- dascore/io/optasense/utils.py
+++ dascore/io/optasense/utils.py
@@ -1,4 +1,4 @@
-"""Utilities for terra15."""
+"""Utilities for Optasense."""
 
 from typing import Optional
 
@@ -13,23 +13,28 @@ from dascore.utils.time import datetime_to_float, to_datetime64
 # --- Getting format/version
 
 
-def _get_terra15_version_str(hdf_fi) -> str:
+def _get_optasense_version_str(hdf_fi) -> str:
     """
-    Return the version string for terra15 file.
+    Return the version string for Optasense file.
     """
 
-    # define a few root attrs that act as a "fingerprint" for terra15 files
+    # define a few root attrs that act as a "fingerprint" for Optasense files
     expected_attrs = [
-        "acoustic_bandwidth_end",
-        "amplifier_incoming_current",
-        "file_start_computer_time",
-        "file_version",
+        "GaugeLength",
+        "VendorCode",
+        "MeasurementStartTime",
+        "schemaVersion",
     ]
-    root_attrs = hdf_fi.root._v_attrs
-    is_terra15 = all([hasattr(root_attrs, x) for x in expected_attrs])
-    if not is_terra15:
+
+    acquisition_group = hdf_fi.get_node("/Acquisition")
+    acquisition_attrs = acquisition_group._v_attrs
+
+    is_optasense = all([hasattr(acquisition_attrs, x) for x in expected_attrs])
+    if not is_optasense:
         return ""
-    return str(root_attrs.file_version)
+    if "OptaSense" not in str(acquisition_attrs.VendorCode.decode()):
+        return "Not Optasense file"
+    return str(acquisition_attrs.schemaVersion.decode())
 
 
 # --- Getting File summaries
@@ -37,7 +42,7 @@ def _get_terra15_version_str(hdf_fi) -> str:
 
 def _get_scanned_time_min_max(data_node):
     """Get the min/max time from time array."""
-    time = data_node["gps_time"]
+    time = 1e-6 * data_node.RawDataTime.read()
     t_len = len(time)
     # first try fast path by tacking first/last of time
     tmin, tmax = time[0], time[-1]
@@ -55,8 +60,8 @@ def _get_extra_scan_attrs(self, file_version, path, data_node):
     """Get the extra attributes that go into summary information."""
     tmin, tmax, _ = _get_scanned_time_min_max(data_node)
     out = {
-        "time_min": to_datetime64(tmin),
-        "time_max": to_datetime64(tmax),
+        "time_min": to_datetime64(tmin.astype(np.float64)),
+        "time_max": to_datetime64(tmax.astype(np.float64)),
         "path": path,
         "file_format": self.name,
         "file_version": str(file_version),
@@ -65,24 +70,24 @@ def _get_extra_scan_attrs(self, file_version, path, data_node):
 
 
 def _get_version_data_node(root):
-    """Get the version, time, and data node from terra15 file."""
-    version = str(root._v_attrs.file_version)
-    if version == "4":
-        data_type = root._v_attrs.data_product
+    """Get the version, time, and data node from Optasense file."""
+    version = str(root._v_attrs.schemaVersion.decode())
+
+    if version == "2.0":
+        data_type = "Raw[0]"
         data_node = root[data_type]
-    elif version == "5":
-        data_node = root["data_product"]
+
     else:
-        raise NotImplementedError("Unknown Terra15 version")
+        raise NotImplementedError("Unknown Optasense version")
     return version, data_node
 
 
-def _scan_terra15(self, fi, path):
-    """Scan a terra15 file, return metadata."""
-    root = fi.root
-    root_attrs = fi.root._v_attrs
+def _scan_optasense(self, fi, path):
+    """Scan an Optasense file, return metadata."""
+    root = fi.get_node("/Acquisition")
+    root_attrs = root._v_attrs
     version, data_node = _get_version_data_node(root)
-    out = _get_default_attrs(data_node.data.attrs, root_attrs)
+    out = _get_default_attrs(data_node, root_attrs)
     out.update(_get_extra_scan_attrs(self, version, path, data_node))
     return [PatchFileSummary.parse_obj(out)]
 
@@ -106,8 +111,8 @@ def _get_start_stop(time_len, time_lims, file_tmin, dt):
 
 
 def _get_dar_attrs(data_node, root, tar, dar):
-    """Get the attributes for the terra15 data array (loaded)"""
-    attrs = _get_default_attrs(data_node.data.attrs, root._v_attrs)
+    """Get the attributes for the Optasense data array (loaded)"""
+    attrs = _get_default_attrs(data_node, root._v_attrs)
     attrs["time_min"] = tar.min()
     attrs["time_max"] = tar.max()
     attrs["distance_min"] = dar.min()
@@ -115,24 +120,25 @@ def _get_dar_attrs(data_node, root, tar, dar):
     return attrs
 
 
-def _read_terra15(
+def _read_optasense(
     root,
     time: Optional[tuple[timeable_types, timeable_types]] = None,
     distance: Optional[tuple[float, float]] = None,
 ) -> Patch:
     """
-    Read a terra15 file.
+    Read an Optasense file.
 
     Notes
     -----
-    The time array is complicated. There is GPS time and Posix time included
+    The time array is complicated. There is GPS time included
     in the file. In version 0.0.6 and less of dascore we just used gps time.
     However, sometimes this results in subsequent samples having a time before
     the previous sample (time did not increase monotonically).
 
     So now, we use the first GPS sample, then the reported dt to calculate
-    time array. The Terra15 folks suggested this is probably the best way to
-    do it.
+    time array.
+
+    We use channel number rather than distance.
     """
     # get time array
     time_lims = tuple(
@@ -159,45 +165,34 @@ def _read_terra15(
     time_ar = to_datetime64(t_float)
     time_inds = (start_ind, stop_ind)
     # get data and sliced distance coord
-    dist_ar = _get_distance_array(root)
+    dist_ar = np.arange(root._v_attrs.NumberOfLoci)
     dslice = get_slice(dist_ar, distance)
     dist_ar_trimmed = dist_ar[dslice]
-    data = data_node.data[slice(*time_inds), dslice]
-    coords = {"time": time_ar, "distance": dist_ar_trimmed}
-    dims = ("time", "distance")
+    data = data_node.RawData[slice(*time_inds), dslice]
+    coords = {"time": time_ar, "channel_number": dist_ar_trimmed}
+    dims = ("time", "channel_number")
     attrs = _get_dar_attrs(data_node, root, time_ar, dist_ar_trimmed)
+
     return Patch(data=data, coords=coords, attrs=attrs, dims=dims)
 
 
-def _get_default_attrs(data_node_attrs, root_node_attrs):
+def _get_default_attrs(data_node, root_node_attrs):
     """
     Return the required/default attributes which can be fetched from attributes.
 
     Note: missing time, distance absolute ranges. Downstream functions should handle
     this.
     """
-    out = dict(dims="time, distance")
+    out = dict(dims="time, channel_number")
     _root_attrs = {
-        "data_product": "data_type",
-        "dx": "d_distance",
-        "serial_number": "instrument_id",
-        "sensing_range_start": "distance_min",
-        "sensing_range_end": "distance_max",
-        "data_product_units": "data_units",
+        "SpatialSamplingInterval": "d_distance",
+        "StartLocusIndex": "distance_min",
+        "NumberOfLoci": "distance_max",
+        "SpatialSamplingIntervalUnit": "distance_units",
     }
     for treble_name, out_name in _root_attrs.items():
         out[out_name] = getattr(root_node_attrs, treble_name)
-
-    out["d_time"] = data_node_attrs.dT
-
+    timing = data_node.RawDataTime.read()
+    out["d_time"] = 1e-6 * (timing[1] - timing[0])
+    out["data_units"] = data_node._v_attrs.RawDataUnit
     return out
-
-
-def _get_distance_array(root):
-    """Get the distance (along fiber) array."""
-    # TODO: At least for the v4 test file, sensing_range_start, sensing_range_stop,
-    # nx, and dx are not consistent, meaning d_min + dx * nx != d_max
-    # so I just used this method. We need to look more into this.
-    attrs = root._v_attrs
-    dist = (np.arange(attrs.nx) * attrs.dx) + attrs.sensing_range_start
-    return dist
--- tests/test_io/test_optasense/test_optasense_core.py
+++ tests/test_io/test_optasense/test_optasense_core.py
@@ -1,25 +1,133 @@
 """
-Tests for optasense format
+Tests for OptasenseV2 format
 """
+import numpy as np
 import pytest
 
 import dascore as dc
-from dascore.utils.downloader import fetch
+from dascore.constants import REQUIRED_DAS_ATTRS
+from dascore.core.schema import PatchFileSummary
+from dascore.io.core import read
+from dascore.io.optasense.core import OptasenseV2
+
+# from dascore.utils.downloader import fetch
+from dascore.utils.misc import register_func
+
+PATCH_FIXTURES = []
 
 
 @pytest.fixture(scope="session")
 def optasense_v2_example_path():
-    """Return the path to the example terra15 file."""
-    out = fetch("opta_sense_quantx_v2.h5")
-    assert out.exists()
+    """Return the path to the example OptasenseV2 file."""
+    # out = fetch("opta_sense_quantx_v2.h5")
+    # assert out.exists()
+    out = "D:/test_data-master/test_data-master/das/opta_sense_quantx_v2.h5"
     return out
 
 
-class TestV2:
-    """Tests for version 2 of optasense."""
+@pytest.fixture(scope="session")
+@register_func(PATCH_FIXTURES)
+def optasense_v2_das_patch(optasense_v2_example_path):
+    """Read the OptasenseV2 data, return contained DataArray"""
 
-    def test_can_read(self, optasense_v2_example_path):
-        """simple read test using dascore.spool."""
-        opta_spool = dc.spool(optasense_v2_example_path)
-        opta_patch = opta_spool[0]
-        assert isinstance(opta_patch, dc.Patch)
+    # OptaS=OptasenseV2()
+    out = read(optasense_v2_example_path, "optasense")[0]
+    attr_time = out.attrs["time_max"]
+    coord_time = out.coords["time"].max()
+    assert attr_time == coord_time
+    return out
+
+
+#
+# class TestV2:
+#     """Tests for version 2 of optasense."""
+#
+#     def test_can_read(self, optasense_v2_example_path):
+#         """simple read test using dascore.spool."""
+#         opta_spool = dc.spool(optasense_v2_example_path)
+#         opta_patch = opta_spool[0]
+#         assert isinstance(opta_patch, dc.Patch)
+
+
+class TestReadOptasenseV2:
+    """Tests for reading the OptasenseV2 format."""
+
+    def test_type(self, optasense_v2_das_patch):
+        """Ensure the expected type is returned."""
+        assert isinstance(optasense_v2_das_patch, dc.Patch)
+
+    def test_attributes(self, optasense_v2_das_patch):
+        """Ensure a few of the expected attrs exist in array."""
+        attrs = dict(optasense_v2_das_patch.attrs)
+        expected_attrs = {"time_min", "time_max", "distance_min", "data_units"}
+        assert set(expected_attrs).issubset(set(attrs))
+
+    def test_has_required_attrs(self, optasense_v2_das_patch):
+        """Ensure the required das attrs are found."""
+        assert set(REQUIRED_DAS_ATTRS).issubset(set(dict(optasense_v2_das_patch.attrs)))
+
+    def test_coord_attr_time_equal(self, optasense_v2_das_patch):
+        """The time reported in the attrs and coords should match."""
+        attr_time = optasense_v2_das_patch.attrs["time_max"]
+        coord_time = optasense_v2_das_patch.coords["time"].max()
+        assert attr_time == coord_time
+
+    def test_time_dist_slice(self, optasense_v2_das_patch, optasense_v2_example_path):
+        """Ensure slicing distance and time works from read func."""
+        time_array = optasense_v2_das_patch.coords["time"]
+        dist_array = optasense_v2_das_patch.coords["channel_number"]
+        t1, t2 = time_array[10], time_array[40]
+        d1, d2 = dist_array[10], dist_array[40]
+        patch = OptasenseV2().read(
+            optasense_v2_example_path, time=(t1, t2), distance=(d1, d2)
+        )[0]
+        attrs, coords = patch.attrs, patch.coords
+        assert attrs["time_min"] == coords["time"].min() == t1
+        assert (attrs["time_max"] - t2) < (attrs["d_time"] / 4)
+        assert attrs["distance_min"] == coords["channel_number"].min() == d1
+        assert attrs["distance_max"] == coords["channel_number"].max() == d2
+
+    def test_no_arrays_in_attrs(self, optasense_v2_das_patch):
+        """
+        Ensure that the attributes are not arrays.
+        Originally, attrs like time_min can be arrays with empty shapes.
+        """
+        for key, val in optasense_v2_das_patch.attrs.items():
+            assert not isinstance(val, np.ndarray)
+
+
+class TestIsOptasenseV2:
+    """Tests for function to determine if a file is an OptasenseV2 file."""
+
+    def test_format_and_version(self, optasense_v2_example_path):
+        """Ensure version two is recognized."""
+        format, version = OptasenseV2().get_format(optasense_v2_example_path)
+        assert format == OptasenseV2.name
+        assert version == OptasenseV2.version
+
+    def test_not_optasensev2_not_df5(self, dummy_text_file):
+        """Test for not even a hdf5 file."""
+        parser = OptasenseV2()
+        assert not parser.get_format(dummy_text_file)
+        assert not parser.get_format(dummy_text_file.parent)
+
+    def test_hdf5file_not_optasensev2(self, generic_hdf5):
+        """Assert that the generic hdf5 file is not a OptasenseV2."""
+        parser = OptasenseV2()
+        assert not parser.get_format(generic_hdf5)
+
+
+class TestScanOptasenseV2:
+    """Tests for scanning OptasenseV2 file."""
+
+    def test_scanning(self, optasense_v2_das_patch, optasense_v2_example_path):
+        """Tests for getting summary info from OptasenseV2 data."""
+        parser = OptasenseV2()
+        out = parser.scan(optasense_v2_example_path)
+        assert isinstance(out, list)
+        assert len(out) == 1
+        assert isinstance(out[0], PatchFileSummary)
+
+        data = out[0]
+        assert data.file_format == parser.name
+        assert data.file_version == parser.version
