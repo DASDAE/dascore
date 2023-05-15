1d2e627a57eedb152d01a8463577d7eb0c0c1fbb
Derrick Chambers <chambers.ja.derrick@gmail.com>
2023-05-15 16:14:11 -0600

    start transformatter

--- dascore/utils/transformatter.py
+++ dascore/utils/transformatter.py
@@ -1,3 +1,16 @@
 """
 Implements logic to apply formatting changes to patches from transformations.
 """
+
+class BaseTransformatter:
+    """
+    Base model for helping to apply transformation format changes.
+    """
+
+class FourierTransformatter(BaseTransformatter):
+    """
+    Formatters
+    """
+    pass
+
+# formaer
