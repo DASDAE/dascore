869fb29516a1c1c007a931c55fd90c462b0b49bd
derrick chambers <djachambeador@gmail.com>
2023-05-18 15:08:28 -0600

    update spectrogram

--- tests/test_transform/test_spectro_transform.py
+++ tests/test_transform/test_spectro_transform.py
@@ -13,14 +13,18 @@ class TestSpectroTransform:
     @pytest.fixture()
     def spec_patch(self, random_patch):
         """Simple patch trasnformed to spectrogram."""
-        return random_patch.tran.spectrogram()
+        return random_patch.tran.spectrogram("time")
 
     def test_spec_patch_dimensions(self, spec_patch, random_patch):
         """Ensure expected dimensions now exist."""
         dims = spec_patch.dims
         # dims should have been added
         assert len(dims) > len(random_patch.dims)
-        assert set(dims) == (set(random_patch.dims) | {"frequency"})
+        assert set(dims) == (set(random_patch.dims) | {"ft_time"})
+        # because of how spectrogram works, the original dim is replaced with
+        # the frequency equivalent, and the original dim is added to the end.
+        time_index = random_patch.dims.index("time")
+        assert spec_patch.dims[time_index] == "ft_time"
 
     def test_time_first(self, random_patch):
         """Ensure the spectrogram still works when time dim is first."""
