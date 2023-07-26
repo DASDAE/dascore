bb5d17c3113cf4cae09ae24a37ea9109bbd8c11a
Derrick Chambers <d-chambers@users.noreply.github.com>
2023-07-25 20:23:40 -0600

    pre-change

---
+++ scripts/find_futures.py
@@ -0,0 +1,40 @@
+"""
+Find all python files which dont have "from __future__ import annotatiosn"
+"""
+from pathlib import Path
+
+
+def has_search_str(path, search_string):
+    """Return True if search string in path"""
+    for line in path.read_text().splitlines():
+        if search_string in line:
+            return True
+    return False
+
+
+def rewrite_file(path, import_str):
+    """Rewrite file to have import statement."""
+    out = path.read_text().splitlines()
+    try:
+        index = out[1:].index('"""')
+    except ValueError:
+        try:
+            index = out[1:].index("'''")
+        except ValueError as e:
+            raise e
+    out.insert(index + 2, import_str)
+    new_str = "\n".join(out)
+    with open(path, "w") as fi:
+        fi.write(new_str)
+
+
+if __name__ == "__main__":
+    search_str = "from __future__ import annotations"
+    base = Path(__file__).parent.parent
+    missing = []
+    for path in base.rglob("*.py"):
+        if not has_search_str(path, search_str):
+            rewrite_file(path, search_str)
+    for path in missing:
+        print(path)
+    breakpoint()
