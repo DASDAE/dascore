2604c76207668972c00fadfdcbf2f9f1b01d0bf1
derrick chambers <djachambeador@gmail.com>
2023-08-21 11:54:15 -0600

    fix #221

--- tests/test_io/test_prodml/test_prod_ml.py
+++ tests/test_io/test_prodml/test_prod_ml.py
@@ -1,7 +1,10 @@
 """Generic tests for prodml support."""
 from __future__ import annotations
 
+import shutil
+
 import pytest
+import tables
 
 import dascore as dc
 from dascore.io.core import read
@@ -35,6 +38,20 @@ class TestProdMLFile:
     """
 
     @pytest.fixture(scope="class")
+    def issue_221_patch_path(self, tmp_path_factory):
+        """Ensure dims are correctly ascertained."""
+        tmp_path = tmp_path_factory.mktemp("issue_221")
+        path = dc.utils.downloader.fetch("prodml_2.0.h5")
+        new_path = shutil.copy2(path, tmp_path / "prod_2_monkey_patched.h5")
+        with tables.open_file(new_path, "a") as fi:
+            # monkey patch dimensions to simulate issue.
+            new_dims = "time, locus"
+            parent_node = fi.root.Acquisition["Raw[0]"]
+            node = parent_node["RawData"]
+            node._v_attrs.Dimensions = new_dims
+        return new_path
+
+    @pytest.fixture(scope="class")
     def silixa_h5_patch(self, idas_h5_example_path):
         """Get the silixa file, return Patch."""
         return dc.spool(idas_h5_example_path)[0]
@@ -48,6 +65,11 @@ class TestProdMLFile:
         """Ensure gauge-length is found in patch attrs."""
         patch = silixa_h5_patch
         assert hasattr(patch.attrs, "gauge_length")
+
+    def test_issue_221(self, issue_221_patch_path):
+        """Ensure dims are correctly ascertained."""
+        patch = dc.read(issue_221_patch_path)[0]
+        assert isinstance(patch, dc.Patch)
 
 
 class TestReadQuantXV2:
