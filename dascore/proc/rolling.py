58ded5687496591cbcf71fdb1365ce4b0e2c0853
ahmadtourei <ahmad9@vt.edu>
2023-08-29 11:20:36 -0600

    fixed a typo in docs

--- dascore/proc/rolling.py
+++ dascore/proc/rolling.py
@@ -233,7 +233,7 @@ def rolling(patch, step=None, center=False, **kwargs):
     >>> new_attrs["min_time"] = np.min(samples)
     >>> new_attrs["max_time"] = np.max(samples)
 
-    >>> patch_mean_no_nan = patch.new_data(
+    >>> patch_mean_no_nan = patch.new(
     ...     data=rolling_mean_values_no_nan,
     ...     attrs=new_attrs,
     ...     dims=patch.dims,
