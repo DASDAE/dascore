db8333b255ae998565b40c51248beb51deef7f82
derrick chambers <djachambeador@gmail.com>
2023-08-30 18:09:51 -0600

    improve rolling import

--- dascore/proc/__init__.py
+++ dascore/proc/__init__.py
@@ -8,7 +8,7 @@ from .coords import *  # noqa
 from .detrend import detrend
 from .filter import median_filter, pass_filter, sobel_filter
 from .resample import decimate, interpolate, iresample, resample
-from .rolling import _NumpyPatchRoller
+from .rolling import rolling
 from .select import iselect, select
 from .taper import taper
 from .units import convert_units, set_units, simplify_units
