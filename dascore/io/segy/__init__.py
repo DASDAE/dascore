dfb930f5b5eeca8466933cddfd58470cecc48938
derrick chambers <djachambeador@gmail.com>
2023-10-03 14:19:18 -0600

    fix init docstring

--- dascore/io/segy/__init__.py
+++ dascore/io/segy/__init__.py
@@ -1,13 +1,20 @@
 """
-RSF format support module.
+SEGY format support module.
 
-Placeholder for interfacing with SEGY formats.
+Notes
+-----
+- Distance information is not found in most SEGY DAS files so returned
+  dimensions are "channel" and "time" rather than "distance" and "time".
 
 Examples
 --------
 import dascore as dc
+from dascore.utils.downloader import fetch
 
-rsfdata = dc.spool('path_to_file.segy')
+# get the path to a segy file.
+path = fetch("conoco_segy_1.sgy")
+
+segy_patch = dc.spool(path)[0]
 """
 
 from .core import SegyV2
