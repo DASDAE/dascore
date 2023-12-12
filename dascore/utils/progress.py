32b2e4d88704cd14479268f664f5da2e37155e71
derrick chambers <djachambeador@gmail.com>
2023-12-12 14:07:57 -0700

    reduce progress bar refresh rate

--- dascore/utils/progress.py
+++ dascore/utils/progress.py
@@ -16,14 +16,18 @@ def track(sequence, description):
         return
     # Normal progress bar behavior
     progress = prog.Progress(
-        prog.SpinnerColumn(),
+        # prog.SpinnerColumn(),
         prog.TextColumn("[progress.description]{task.description}"),
         prog.BarColumn(bar_width=30),
         prog.TaskProgressColumn(),
         prog.TimeRemainingColumn(),
         prog.TimeElapsedColumn(),
         prog.MofNCompleteColumn(),
+        refresh_per_second=1,
     )
+    # Note: we rest the refresh-rate low because of latency issues with
+    # remote servers. It also helped to remove the spinner so the bar didnt
+    # look so jumpy. See #305.
     total = len(sequence)
     with progress:
         yield from progress.track(
--- tests/test_utils/test_progress.py
+++ tests/test_utils/test_progress.py
@@ -9,7 +9,8 @@ class TestProgressBar:
     """Tests for the rich progress bar."""
 
     def test_progressbar_shows(self, monkeypatch):
-        """Undo debug patch to progress bar shows."""
+        """A test for when the progress bar shows to run progress logic."""
+        # Undo debug patch so progress bar shows.
         monkeypatch.setattr(dc, "_debug", False)
         for _ in track([1, 2, 3], "testing_tracker"):
             pass
