0c62e4a02068d1f5372714954b038c73aae8ebc2
Derrick Chambers <d-chambers@users.noreply.github.com>
2023-12-18 11:25:58 -0700

    try adding test script

---
+++ .github/test_code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to run tests to account for wonkiness of periodic mac failures.
+args=" -s --cov dascore --cov-append --cov-report=xml"
+if [[ "$1" == "doctest" ]]; then
+  args="dascore --doctest-modules"
+fi
+
+exit_code=0
+
+python -m pytest $args || exit_code=$?
+
+# Check the exit code is related to sporadic failures on mac, see #312
+if [ $exit_code -ne 132 ] && [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
