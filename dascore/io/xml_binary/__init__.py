472f7006d4e7a7b7109a4cba34e3c5490d219ae9
derrick chambers <djachambeador@gmail.com>
2024-06-04 10:01:09 -0600

    add docs about directory FiberIO

--- dascore/io/xml_binary/__init__.py
+++ dascore/io/xml_binary/__init__.py
@@ -1,9 +1,15 @@
 """
-Module for reading data stored in binary raw format.
+Module for reading data stored in xml_binary format.
 
-Notes
------
-The metadata is stored in each data directory in an XML file.
+This format is a directory which contains metadata as a single xml file
+as well as any number of binary files (raw numeric buffers) which contain
+information about their start time in the file name. An example directory
+might look like this:
+
+data_folder
+   metadata.xml
+   DAS_20240530T011500_000000Z.raw
+   DAS_20240530T011501_000000Z.raw
 """
 from __future__ import annotations
 from .core import XMLBinaryV1
