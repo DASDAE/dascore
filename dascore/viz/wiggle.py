b72962fdf87ae629d4baa3c38ffbfb2ae9023b9d
ahmadtourei <ahmadtourei@gmail.com>
2024-06-05 17:36:02 -0400

    dtype_time_like to exclude time dim in wiggle

--- dascore/viz/wiggle.py
+++ dascore/viz/wiggle.py
@@ -11,6 +11,7 @@ from dascore.utils.plotting import (
     _get_ax,
     _get_dim_label,
 )
+from dascore.utils.time import dtype_time_like
 
 
 def _get_offsets_factor(patch, dim, scale, other_labels):
@@ -40,7 +41,7 @@ def _shade(offsets, ax, data_scaled, color, wiggle_labels):
 def _format_y_axis_ticks(ax, offsets, other_axis_ticks, max_ticks=10):
     """Format the Y axis tick labels."""
     # make sure not printing too many digits on the figure
-    if other_axis_ticks.dtype != np.dtype("<M8[ns]"):
+    if not dtype_time_like(other_axis_ticks):
         other_axis_ticks = np.around(other_axis_ticks, decimals=2)
     # set the offset
     ax.set_yticks(offsets, other_axis_ticks)
