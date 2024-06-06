bca67c4369866cf61921b9ff158b929315152e8d
Abdul Hafiz Saeed Issah <issahhafiz@gmail.com>
2024-06-06 17:42:40 -0600

    Map plot (#385)

--- dascore/viz/__init__.py
+++ dascore/viz/__init__.py
@@ -7,6 +7,7 @@ from dascore.utils.misc import MethodNameSpace
 from .spectrogram import spectrogram
 from .waterfall import waterfall
 from .wiggle import wiggle
+from .map_fiber import map_fiber
 
 
 class VizPatchNameSpace(MethodNameSpace):
@@ -15,3 +16,4 @@ class VizPatchNameSpace(MethodNameSpace):
     waterfall = waterfall
     spectrogram = spectrogram
     wiggle = wiggle
+    map_fiber = map_fiber
