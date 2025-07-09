b816be7f722e75e57397236f142d084624e2107d
Shihao-Yuan <38784416+Shihao-Yuan@users.noreply.github.com>
2025-07-08 19:13:08 -0600

    Add OptoDAS I/O format V10 (#506)
    * adding Sentek test data
    
    * Add sentek data format
    
    * new contributor added
    
    * contributor added
    
    * optoDAS format update
    
    * merge to master
    
    * Add: extend OptoDAS I/O format to V 10

--- dascore/io/optodas/__init__.py
+++ dascore/io/optodas/__init__.py
@@ -6,4 +6,4 @@ This is used by the OptoDAS interrogator made by Alcatel Submarine Networks.
 More info here: https://web.asn.com/
 """
 from __future__ import annotations
-from .core import OptoDASV8
+from .core import OptoDASV8, OptoDASV10
