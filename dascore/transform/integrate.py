e5208e563fce3625bc798b0e59b05292ede0735b
derrick chambers <djachambeador@gmail.com>
2025-12-22 09:12:05 -0700

    fix trapz issue

--- dascore/transform/integrate.py
+++ dascore/transform/integrate.py
@@ -45,7 +45,8 @@ def _get_definite_integral(patch, dxs_or_vals, dims, axes):
     ndims = len(patch.shape)
     for dxs_or_val, ax in zip(dxs_or_vals, axes):
         # Numpy 2/3 compat code
-        trap = getattr(np, "trapezoid", getattr(np, "trapz"))
+        trap_name = "trapezoid" if hasattr(np, "trapezoid") else "trapz"
+        trap = getattr(np, trap_name)
         indexer = broadcast_for_index(ndims, ax, None, fill=slice(None))
         if is_array(dxs_or_val):
             array = trap(array, x=dxs_or_val, axis=ax)[indexer]
