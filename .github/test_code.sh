764440e1e262db9306db8836d0a334683a553a93
Derrick Chambers <chambers.ja.derrick@gmail.com>
2026-07-12 18:17:42 +0200

    CI: isolate network tests from required suite

--- .github/test_code.sh
+++ .github/test_code.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Script to run tests to account for wonkiness of periodic mac failures.
-args="tests -s --cov dascore --cov-append --cov-report=xml"
+args=(tests -m "not network" -s --cov dascore --cov-append --cov-report=xml)
+if [[ "$1" == "network" ]]; then
+  args=(tests -m network -s)
+fi
 if [[ "$1" == "doctest" ]]; then
-  args="dascore --doctest-modules"
+  args=(dascore --doctest-modules)
 fi
 if [[ "$1" == "profile" ]]; then
-  args="benchmarks --codspeed"
+  args=(benchmarks --codspeed)
 fi
 
 exit_code=0
 
-python -m pytest $args || exit_code=$?
+python -m pytest "${args[@]}" || exit_code=$?
 
 # Check the exit code is related to sporadic failures on mac, see #312
 if [ $exit_code -ne 132 ] && [ $exit_code -ne 0 ]; then
